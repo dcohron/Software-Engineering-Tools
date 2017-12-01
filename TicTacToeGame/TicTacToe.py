f40116474373a1712b55e9e14546d57409ce48ca
dcohron <dcohron@comcast.net>
2017-12-01 10:43:34 -0500

    Update
    Update with testing and continued play

--- TicTacToeGame/TicTacToe.py
+++ TicTacToeGame/TicTacToe.py
@@ -4,7 +4,7 @@ Project:    Optional Practice Assignment
 Professor:  James Rowland
 Author:     Nick Cohron
 Date:       24 November 2017
-Brief:      Tic Tac Toe rendered to the web.
+Brief:      Tic Tac Toe rendered to the console (and soon to the web).
 """
 
 
@@ -12,13 +12,26 @@ Brief:      Tic Tac Toe rendered to the web.
 # Goto 127.0.0.1:5000 (root) to see rendered web page
 
 
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 import random
 import itertools
 from collections import defaultdict
 from prettytable import PrettyTable as PT
+import unittest
+
 
 # app = Flask(__name__)
+
+
+class HomeworkTest(unittest.TestCase):
+    """Unit tests for this script."""
+
+    def test_check_win(self):
+        """Verify that function 'check_win' is functioning properly."""
+        test_board = TTTGame(3)
+
+        self.assertEqual(test_board.check_win(), (False, 'No winner'))
+
 
 class TTTGame():
     """Class to contain data structures and methods for Tic Tac Toe game."""
@@ -36,15 +49,6 @@ class TTTGame():
         for x, y in itertools.product(range(self.size), range(self.size)):
             board_rep = board_rep + '[' + str(x) +',' + str(y) +']=' + self.board[x][y] + " "
         return board_rep
-
-    # @app.route('/')
-    def display_board(self):
-        """Function to present the board to Flask for web presentation."""
-        return render_template('game_board.html',
-                                   title='Tic Tac Toe',
-                                   table_title="The Game Board",
-                                   size=self.size,
-                                   board=self.board)
 
     def print_board(self):
         """Function to print out a pretty table of the game board."""
@@ -127,6 +131,11 @@ class TTTGame():
         return (False, 'No winner')
 
 
+# global variables required by Flask
+# board_size = 0
+# board = TTTGame(0)
+
+
 def get_size_input():
     """Function takes no input parameters, gets user input as a string and returns that string.
     Does not need to use 'try' and 'except' as it casts string to integer, if fail on cast
@@ -136,12 +145,22 @@ def get_size_input():
     print()
     input_size = input("Please enter a single integer for size of board ('3' will yield 3x3 board): ")
 
-    # now check that input was correct, else recursive call
+    # check that input was correct, else recursive call
     if not type(int(input_size)) == int:
         print("ERROR: Not an integer, please try again.")
         input_size = get_size()
 
     return int(input_size)
+
+
+# @app.route('/')
+# def display_board():
+#     """Function to present the board to Flask for web presentation."""
+#     return render_template('game_board.html',
+#                                title='Tic Tac Toe',
+#                                table_title="The Game Board",
+#                                size=board_size,
+#                                board=board)
 
 
 def main():
@@ -154,15 +173,12 @@ def main():
 
     # initialize game board
     board = TTTGame(board_size)
-    # print(board)
+    print(board)
     print()
 
     # output results to console
     board.print_board()
     print()
-
-    # output results to web
-    # board.display_board()
 
     # check for win on board
     win_bool, location = board.check_win()
@@ -171,12 +187,24 @@ def main():
     else:
         print("No winner this time.")
 
+    # output results to web
+    # app.run(debug=True)  # automatically restart web server if anything changes
+
+    # replay or exit
+    try:
+        again = input('Would you like to play again? y/n ')
+        if again[0].lower() == 'y':
+            main()
+        else:
+            print('Thank you for playing.')
+    except TypeError:
+        print('ERROR: type error.  Closing program.')
+
     return
 
 
 if __name__ == "__main__":
     # do the main program loop
-    # app.run(debug=True)  # automatically restart webserver if anything changes
     main()
 
     # do unit tests
